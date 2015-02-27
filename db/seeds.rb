34d538602aed68a162eb1e675079ebd2c87dc938
John Moon <john.moon@livingsocial.com>
2015-02-26 23:44:15 -0500

    Create seed data for quote me app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,291 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Quote.create([
+  {:name => "Franklin Roosevelt", :phrase =>"The only limit to our realization
+  of tomorrow will be our doubts of today. Let us move forward with strong and
+  active faith."},
+  {:name => "Franklin Roosevelt", :phrase =>"If you treat people right they will
+  treat you right - ninety percent of the time."},
+  {:name => "Franklin Roosevelt", :phrase =>"It is common sense to take a method
+  and try it. If it fails, admit it frankly and try another. But above all, try
+  something."},
+  {:name => "Franklin Roosevelt", :phrase =>"Repetition does not transform a lie
+  into a truth."},
+  {:name => "Franklin Roosevelt", :phrase =>"Men are not prisoners of fate, but
+  only prisoners of their own minds."},
+  {:name => "Franklin Roosevelt", :phrase =>"The test of our progress is not
+  whether we add more to the abundance of those who have much; it is whether we
+  provide enough for those who have too little."},
+  {:name => "Franklin Roosevelt", :phrase =>"When you get to the end of your
+  rope, tie a knot and hang on."},
+  {:name => "Franklin Roosevelt", :phrase =>"Confidence... thrives on honesty,
+  on honor, on the sacredness of obligations, on faithful protection and on
+  unselfish performance. Without them it cannot live."},
+  {:name => "Franklin Roosevelt", :phrase =>"Let me assert my firm belief that
+  the only thing we have to fear is fear itself."},
+  {:name => "Franklin Roosevelt", :phrase =>"We cannot always build the future
+  for our youth, but we can build our youth for the future."},
+  {:name => "Abraham Lincoln", :phrase =>"In the end, it's not the years in your
+  life that count. It's the life in your years."},
+  {:name => "Abraham Lincoln", :phrase =>"All that I am, or hope to be, I owe to
+  my angel mother."},
+  {:name => "Abraham Lincoln", :phrase =>"Most folks are as happy as they make
+  up their minds to be."},
+  {:name => "Abraham Lincoln", :phrase =>"Nearly all men can stand adversity,
+  but if you want to test a man's character, give him power."},
+  {:name => "Abraham Lincoln", :phrase =>"No man has a good enough memory to be
+  a successful liar."},
+  {:name => "Abraham Lincoln", :phrase =>"You cannot escape the responsibility
+  of tomorrow by evading it today."},
+  {:name => "Abraham Lincoln", :phrase =>"Give me six hours to chop down a tree
+  and I will spend the first four sharpening the axe."},
+  {:name => "Abraham Lincoln", :phrase =>"Better to remain silent and be thought
+  a fool than to speak out and remove all doubt."},
+  {:name => "Abraham Lincoln", :phrase =>"Whatever you are, be a good one."},
+  {:name => "Abraham Lincoln", :phrase =>"No man is good enough to govern
+  another man without the other's consent."},
+  {:name => "Abraham Lincoln", :phrase =>"Those who deny freedom to others
+  deserve it not for themselves."},
+  {:name => "George Washington", :phrase =>"If we are wise, let us prepare for
+  the worst."},
+  {:name => "George Washington", :phrase =>"Be courteous to all, but intimate
+  with few, and let those few be well tried before you give them your
+  confidence."},
+  {:name => "George Washington", :phrase =>"Friendship is a plant of slow growth
+  and must undergo and withstand the shocks of adversity before it is entitled
+  to the appellation. "},
+  {:name => "George Washington", :phrase =>"Worry is the interest paid by those
+  who borrow trouble."},
+  {:name => "George Washington", :phrase =>"Liberty, when it begins to take
+  root, is a plant of rapid growth."},
+  {:name => "George Washington", :phrase =>"It is better to offer no excuse than
+  a bad one."},
+  {:name => "George Washington", :phrase =>"Few men have virtue to withstand the
+  highest bidder."},
+  {:name => "George Washington", :phrase =>"Observe good faith and justice
+  toward all nations. Cultivate peace and harmony with all."},
+  {:name => "George Washington", :phrase =>"Reason, too late perhaps, may
+  convince you of the folly of misspending time."},
+  {:name => "George Washington", :phrase =>"Ninety-nine percent of the failures
+  come from people who have the habit of making excuses."},
+  {:name => "Barack Obama", :phrase =>"Change will not come if we wait for some
+  other person or some other time. We are the ones we've been waiting for. We
+  are the change that we seek."},
+  {:name => "Barack Obama", :phrase =>"If you're walking down the right path and
+  you're willing to keep walking, eventually you'll make progress."},
+  {:name => "Barack Obama", :phrase =>"The future rewards those who press on. I
+  don't have time to feel sorry for myself. I don't have time to complain. I'm
+  going to press on."},
+  {:name => "Barack Obama", :phrase =>"We need to steer clear of this poverty of
+  ambition, where people want to drive fancy cars and wear nice clothes and live
+  in nice apartments but don't want to work hard to accomplish these things.
+  Everyone should try to realize their full potential."},
+  {:name => "Barack Obama", :phrase =>"That's the good thing about being
+  president, I can do whatever I want."},
+  {:name => "Barack Obama", :phrase =>"Money is not the only answer, but it
+  makes a difference."},
+  {:name => "Barack Obama", :phrase =>"It was the labor movement that helped
+  secure so much of what we take for granted today. The 40-hour work week, the
+  minimum wage, family leave, health insurance, Social Security, Medicare,
+  retirement plans. The cornerstones of the middle-class security all bear the
+  union label."},
+  {:name => "Barack Obama", :phrase =>"If the people cannot trust their
+  government to do the job for which it exists - to protect them and to promote
+  their common welfare - all else is lost."},
+  {:name => "Barack Obama", :phrase =>"After a century of striving, after a year
+  of debate, after a historic vote, health care reform is no longer an unmet
+  promise. It is the law of the land."},
+  {:name => "Barack Obama", :phrase =>"We didn't become the most prosperous
+  country in the world just by rewarding greed and recklessness. We didn't come
+  this far by letting the special interests run wild. We didn't do it just by
+  gambling and chasing paper profits on Wall Street. We built this country by
+  making things, by producing goods we could sell."},
+  {:name => "Thomas Jefferson", :phrase =>"Do you want to know who you are?
+  Don't ask. Act! Action will delineate and define you."},
+  {:name => "Thomas Jefferson", :phrase =>"Nothing can stop the man with the
+  right mental attitude from achieving his goal; nothing on earth can help the
+  man with the wrong mental attitude."},
+  {:name => "Thomas Jefferson", :phrase =>"We hold these truths to be
+  self-evident: that all men are created equal; that they are endowed by their
+  Creator with certain unalienable rights; that among these are life, liberty,
+  and the pursuit of happiness."},
+  {:name => "Thomas Jefferson", :phrase =>"Honesty is the first chapter in the
+  book of wisdom."},
+  {:name => "Thomas Jefferson", :phrase =>"Our greatest happiness does not
+  depend on the condition of life in which chance has placed us, but is always
+  the result of a good conscience, good health, occupation, and freedom in all
+  just pursuits."},
+  {:name => "Thomas Jefferson", :phrase =>"In matters of style, swim with the
+  current; in matters of principle, stand like a rock."},
+  {:name => "Thomas Jefferson", :phrase =>"The glow of one warm thought is to me
+  worth more than money."},
+  {:name => "Thomas Jefferson", :phrase =>"The tree of liberty must be refreshed
+  from time to time with the blood of patriots and tyrants."},
+  {:name => "Thomas Jefferson", :phrase =>"I like the dreams of the future
+  better than the history of the past."},
+  {:name => "Thomas Jefferson", :phrase =>"A Bill of Rights is what the people
+  are entitled to against every government, and what no just government should
+  refuse, or rest on inference."},
+  {:name => "Thomas Jefferson", :phrase =>"Educate and inform the whole mass of
+  the people... They are the only sure reliance for the preservation of our
+  liberty."},
+  {:name => "Andrew Jackson", :phrase =>"There is no pleasure in having nothing
+  to do; the fun is having lots to do and not doing it."},
+  {:name => "Andrew Jackson", :phrase =>"You must pay the price if you wish to
+  secure the blessing."},
+  {:name => "Andrew Jackson", :phrase =>"I weep for the liberty of my country
+  when I see at this early day of its successful experiment that corruption has
+  been imputed to many members of the House of Representatives, and the rights
+  of the people have been bartered for promises of office."},
+  {:name => "Andrew Jackson", :phrase =>"Every good citizen makes his country's
+  honor his own, and cherishes it not only as precious but as sacred. He is
+  willing to risk his life in its defense and its conscious that he gains
+  protection while he gives it."},
+  {:name => "Andrew Jackson", :phrase =>"It is to be regretted that the rich and
+  powerful too often bend the acts of government to their own selfish purposes."},
+  {:name => "Andrew Jackson", :phrase =>"Take time to deliberate; but when the
+  time for action arrives, stop thinking and go in."},
+  {:name => "Andrew Jackson", :phrase =>"I've got big shoes to fill. This is my
+  chance to do something. I have to seize the moment."},
+  {:name => "Andrew Jackson", :phrase =>"Americans are not a perfect people, but
+  we are called to a perfect mission."},
+  {:name => "Andrew Jackson", :phrase =>"I have always been afraid of banks."},
+  {:name => "Andrew Jackson", :phrase =>"There are no necessary evils in
+  government. Its evils exist only in its abuses."},
+  {:name => "Theodore Roosevelt", :phrase =>"Believe you can and you're halfway
+  there."},
+  {:name => "Theodore Roosevelt", :phrase =>"In any moment of decision, the best
+  thing you can do is the right thing, the next best thing is the wrong thing,
+  and the worst thing you can do is nothing."},
+  {:name => "Theodore Roosevelt", :phrase =>"If you could kick the person in
+  the pants responsible for most of your trouble, you wouldn't sit for a month."},
+  {:name => "Theodore Roosevelt", :phrase =>"Far better is it to dare mighty
+  things, to win glorious triumphs, even though checkered by failure... than to
+  rank with those poor spirits who neither enjoy nor suffer much, because they
+  live in a gray twilight that knows not victory nor defeat."},
+  {:name => "Theodore Roosevelt", :phrase =>"With self-discipline most anything
+  is possible."},
+  {:name => "Theodore Roosevelt", :phrase =>"Far and away the best prize that
+  life has to offer is the chance to work hard at work worth doing."},
+  {:name => "Theodore Roosevelt", :phrase =>"A man who has never gone to school
+  may steal from a freight car; but if he has a university education, he may
+  steal the whole railroad."},
+  {:name => "Theodore Roosevelt", :phrase =>"People ask the difference between a
+  leader and a boss. The leader leads, and the boss drives."},
+  {:name => "Theodore Roosevelt", :phrase =>"It is only through labor and
+  painful effort, by grim energy and resolute courage, that we move on to better
+  things."},
+  {:name => "Theodore Roosevelt", :phrase =>"Speak softly and carry a big stick;
+  you will go far."},
+  {:name => "Woodrow Wilson", :phrase =>"You are not here merely to make a
+  living. You are here in order to enable the world to live more amply, with
+  greater vision, with a finer spirit of hope and achievement. You are here to
+  enrich the world, and you impoverish yourself if you forget the errand."},
+  {:name => "Woodrow Wilson", :phrase =>"The world must be made safe for
+  democracy. Its peace must be planted upon the tested foundations of political
+  liberty."},
+  {:name => "Woodrow Wilson", :phrase =>"I not only use all the brains that I
+  have, but all that I can borrow."},
+  {:name => "Woodrow Wilson", :phrase =>"There is no higher religion than human
+  service. To work for the common good is the greatest creed."},
+  {:name => "Woodrow Wilson", :phrase =>"The only use of an obstacle is to be
+  overcome. All that an obstacle does with brave men is, not to frighten them,
+  but to challenge them."},
+  {:name => "Woodrow Wilson", :phrase =>"The government, which was designed for
+  the people, has got into the hands of the bosses and their employers, the
+  special interests. An invisible empire has been set up above the forms of
+  democracy."},
+  {:name => "Woodrow Wilson", :phrase =>"The man who is swimming against the
+  stream knows the strength of it."},
+  {:name => "Woodrow Wilson", :phrase =>"f you want to make enemies, try to
+  change something."},
+  {:name => "Woodrow Wilson", :phrase =>"Golf is a game in which one endeavors
+  to control a ball with implements ill adapted for the purpose."},
+  {:name => "Woodrow Wilson", :phrase =>"At every crisis in one's life, it is
+  absolute salvation to have some sympathetic friend to whom you can think aloud
+  without restraint or misgiving."},
+  {:name => "Harry Truman", :phrase =>"It is amazing what you can accomplish if
+  you do not care who gets the credit."},
+  {:name => "Harry Truman", :phrase =>"America was not built on fear. America
+  was built on courage, on imagination and an unbeatable determination to do the
+  job at hand."},
+  {:name => "Harry Truman", :phrase =>"The atom bomb was no 'great decision.' It
+  was merely another powerful weapon in the arsenal of righteousness."},
+  {:name => "Harry Truman", :phrase =>"Men make history and not the other way
+  around. In periods where there is no leadership, society stands still.
+  Progress occurs when courageous, skillful leaders seize the opportunity to
+  change things for the better."},
+  {:name => "Harry Truman", :phrase =>"Actions are the seed of fate deeds grow
+  into destiny."},
+  {:name => "Harry Truman", :phrase =>"Carry the battle to them. Don't let them
+  bring it to you. Put them on the defensive and don't ever apologize for
+  anything."},
+  {:name => "Harry Truman", :phrase =>"In reading the lives of great men, I
+  found that the first victory they won was over themselves... self-discipline
+  with all of them came first."},
+  {:name => "Harry Truman", :phrase =>"The reward of suffering is experience."},
+  {:name => "Harry Truman", :phrase =>"A pessimist is one who makes difficulties
+  of his opportunities and an optimist is one who makes opportunities of his
+  difficulties."},
+  {:name => "Harry Truman", :phrase =>"The United Nations is designed to make
+  possible lasting freedom and independence for all its members."},
+  {:name => "James Polk", :phrase =>"No president who performs his duties
+  faithfully and conscientiously can have any leisure."},
+  {:name => "James Polk", :phrase =>"Well may the boldest fear and the wisest
+  tremble when incurring responsibilities on which may depend our country's
+  peace and prosperity, and in some degree the hopes and happiness of the whole
+  human family."},
+  {:name => "James Polk", :phrase =>"One great object of the Constitution was to
+  restrain majorities from oppressing minorities or encroaching upon their just
+  rights."},
+  {:name => "James Polk", :phrase =>"The passion for office among members of
+  Congress is very great, if not absolutely disreputable, and greatly
+  embarrasses the operations of the Government. They create offices by their own
+  votes and then seek to fill them themselves."},
+  {:name => "James Polk", :phrase =>"Peace, plenty, and contentment reign
+  throughout our borders, and our beloved country presents a sublime moral
+  spectacle to the world."},
+  {:name => "James Polk", :phrase =>"There is more selfishness and less
+  principle among members of Congress than I had any conception of, before I
+  became President of the U.S."},
+  {:name => "James Polk", :phrase =>"I am heartily rejoiced that my term is so
+  near its close. I will soon cease to be a servant and will become a sovereign."},
+  {:name => "James Polk", :phrase =>"The world has nothing to fear from military
+  ambition in our Government."},
+  {:name => "James Polk", :phrase =>"Foreign powers do not seem to appreciate
+  the true character of our government."},
+  {:name => "James Polk", :phrase =>"Minorities have a right to appeal to the
+  Constitution as a shield against such oppression."},
+  {:name => "Dwight Eisenhower", :phrase =>"I hate war as only a soldier who has
+  lived it can, only as one who has seen its brutality, its futility, its
+  stupidity."},
+  {:name => "Dwight Eisenhower", :phrase =>"Neither a wise man nor a brave man
+  lies down on the tracks of history to wait for the train of the future to run
+  over him."},
+  {:name => "Dwight Eisenhower", :phrase =>"This world of ours... must avoid
+  becoming a community of dreadful fear and hate, and be, instead, a proud
+  confederation of mutual trust and respect."},
+  {:name => "Dwight Eisenhower", :phrase =>"Motivation is the art of getting
+  people to do what you want them to do because they want to do it."},
+  {:name => "Dwight Eisenhower", :phrase =>"A people that values its privileges
+  above its principles soon loses both."},
+  {:name => "Dwight Eisenhower", :phrase =>"Plans are nothing; planning is
+  everything."},
+  {:name => "Dwight Eisenhower", :phrase =>"Every gun that is made, every
+  warship launched, every rocket fired, signifies in the final sense a theft
+  from those who hunger and are not fed, those who are cold and are not clothed."},
+  {:name => "Dwight Eisenhower", :phrase =>"Should any political party attempt
+  to abolish social security, unemployment insurance, and eliminate labor laws
+  and farm programs, you would not hear of that party again in our political
+  history."},
+  {:name => "Dwight Eisenhower", :phrase =>"We are going to have peace even if
+  we have to fight for it."},
+  {:name => "Dwight Eisenhower", :phrase =>"The supreme quality for leadership
+  is unquestionably integrity. Without it, no real success is possible, no
+  matter whether it is on a section gang, a football field, in an army, or in an
+  office."}
+  ])
+
--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -9,6 +9,7 @@ class QuotesControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:quotes)
+    assert_equal 'what', response
   end
 
   test "should get new" do
